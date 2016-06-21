b0a7e6fdb52c43b1484ec18d71738c7b18ea8e51
David Pavitt <dpavitt@gmail.com>
2016-06-21 17:42:06 +0100

    Got results from WebTag hits

--- dev/data_extraction_demo.R
+++ dev/data_extraction_demo.R
@@ -1,29 +1,54 @@
 library(RGoogleAnalytics)
 
-# Authorize the Google Analytics account
-# This need not be executed in every session once the token object is created 
-# and saved
-token <- Auth(client.id = "246473626117-tvaopdf65gincc6k4enc8arhm9jasihc.apps.googleusercontent.com",
-              client.secret = "Ju-EnTxewNMzSvoo92X9koKm")
-
-# Save the token object for future sessions
-save(token,file="./token_file")
-
-# In future sessions it can be loaded by running load("./token_file")
-
+load("token_file")
 ValidateToken(token)
 
+test_url = "/government/publications/webtag-tag-unit-a1-1-cost-benefit-analysis-november-2014"
+test_url = "/government/collections/tempro"
+page_filter = paste0("ga:pagePath==", test_url)
+
+
 # Build a list of all the Query Parameters
-query.list <- Init(start.date = "2013-11-28",
-                   end.date = "2013-12-04",
-                   dimensions = "ga:date,ga:pagePath,ga:hour,ga:medium",
+#Valid options for metrics and dimensions are given at:
+#https://developers.google.com/analytics/devguides/reporting/core/dimsmets
+
+# ga:pagePath string of the url after gov.uk
+# ga:pageviews integer giving the number of pageviews
+# ga:city string city from their IP address
+# ga:browser
+# ga:hour string two digit hour 00 -23
+
+#The table.id was found by running GetProfiles(token)
+
+#I have added the filters. Filter to the selected page only
+top_10_june_20 <- Init(start.date = "2016-06-20",
+                   end.date = "2016-06-20",
+                   dimensions = "ga:pagePath,ga:pageTitle, ga:date",
                    metrics = "ga:sessions,ga:pageviews",
-                   max.results = 10000,
-                   sort = "-ga:date",
-                   table.id = "ga:33093633")
+                   #filters = page_filter,
+                   max.results = 10,
+                   sort = "-ga:pageviews",
+                   table.id = "ga:53872948") #
 
 # Create the Query Builder object so that the query parameters are validated
-ga.query <- QueryBuilder(query.list)
+ga.query <- QueryBuilder(top_10_june_20)
 
 # Extract the data and store it in a data-frame
-ga.data <- GetReportData(ga.query, token, split_daywise = T, delay = 5)
+#ga.data <- GetReportData(ga.query, token, split_daywise = T, delay = 5)
+ga.data <- GetReportData(ga.query, token, split_daywise = F)
+
+#==============================================================
+#Test the ability to filter
+#I have added the filters. Filter to the selected page only
+top_10_june_20 <- Init(start.date = "2016-05-01",
+                       end.date = "2016-05-16",
+                       dimensions = "ga:date, ga:pageTitle",
+                       metrics = "ga:sessions,ga:pageviews",
+                       filters = page_filter,
+                       max.results = 10000,
+                       sort = "-ga:date",
+                       table.id = "ga:53872948") #
+ga.query <- QueryBuilder(top_10_june_20)
+ga.data <- GetReportData(ga.query, token, split_daywise = T)
+
+#Get an error if there are no page views on a certain date, will have to loop each date manually
